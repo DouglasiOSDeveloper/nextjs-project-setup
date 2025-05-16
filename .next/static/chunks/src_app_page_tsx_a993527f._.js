e9a8d20b16237a9013d010c5f0fa0e8d4deb2414
Douglas Figueirôa <62218512+DouglasiOSDeveloper@users.noreply.github.com>
2025-05-16 08:39:09 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747395548605

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_527a7ed1._.js",
+  "static/chunks/node_modules_cbe4e28f._.js"
+],
+    source: "dynamic"
+});
